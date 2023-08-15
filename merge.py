6825a8936cb1d3178519db37e83fad70b43ca1b2
mouneem <mounim1024@gmail.com>
2023-08-15 17:46:48 +0200

    calling script from cmd

--- merge.py
+++ merge.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import sys
 
+input_file = sys.argv[1]
+database_file = sys.argv[2]
+output_file = sys.argv[3]
 # read csv file : database
-database = pd.read_csv('./database.csv',sep='\t')
+database = pd.read_csv(database_file,sep='\t')
 # rename columns in database
 database.rename(columns={database.columns[1]: 'REF'},inplace=True)
 database.rename(columns={database.columns[2]: 'ALT'},inplace=True)
@@ -9,10 +13,10 @@ database.rename(columns={database.columns[6]: 'CHROM'},inplace=True)
 database.rename(columns={database.columns[7]: 'POS'},inplace=True)
 database.rename(columns={database.columns[8]: 'SYMBOL'},inplace=True)
 #read csv file : input
-input = pd.read_csv('./input.csv',sep='\t')
+input = pd.read_csv(input_file,sep='\t')
 
 # list of common columns to use for merge ['REF','ALT','CHROM','POS','SYMBOL']
 df = pd.merge(database, input, on = ['REF','ALT','CHROM','POS','SYMBOL'])
 
 # export results
-df.to_csv('database_input.csv')
+df.to_csv(output_file)
